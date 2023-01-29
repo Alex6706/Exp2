df66bcc8024c86372a0d9ad0c4be9dbf053bd4a2
Alex6706 <kvslanz@gmail.com>
2023-01-29 19:56:16 +0300

    initial commit

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿
+
+    
+        Console.Write("Введите первое число ");
+        double a = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Введите второе число ");
+        double b = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Введите третье число ");
+        double d = Convert.ToInt32(Console.ReadLine());
+        double max = Convert.ToInt32(0);
+
+        if (a > b)
+        {
+            max = a;
+        }
+
+        else
+        {
+            max = b;
+        }
+
+        if (max > d)
+        {
+
+        }
+
+        else
+        {
+            max = d;
+        }
+
+        Console.WriteLine($"max = {max}");
+    
